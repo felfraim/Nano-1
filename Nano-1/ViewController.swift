43a435f3481572612e1f02b5c32d5af47cba42a8
josh <joshia.efraim@binus.ac.id>
2022-04-27 15:02:35 +0700

    Initial Commit

---
+++ Nano-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano-1
+//
+//  Created by Joshia Felim Efraim on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
